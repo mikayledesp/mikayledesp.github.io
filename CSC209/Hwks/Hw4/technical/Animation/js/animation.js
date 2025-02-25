407307fcf5c1d47ad9e6a123fba74943faf2e025
Mikayle Despaigne <158209632+mikayledesp@users.noreply.github.com>
2025-02-25 10:54:06 -0500

    got squares moving

--- CSC209/Hwks/Hw4/technical/Animation/js/animation.js
+++ CSC209/Hwks/Hw4/technical/Animation/js/animation.js
@@ -18,4 +18,23 @@ function moveRed()
     }
 }
 
+function moveBlue(){
+    
+    var blueSquare = document.getElementById("blueSq");
+    var bluePos = 350;
+    // using the setInterval to increase or decrease speed based on users option choice 
+    var stepBlueId = setInterval(stepBlue, 10);
+
+    function stepBlue(){
+        if (bluePos == 0){
+            clearInterval(stepBlueId);   
+        } else{
+            // decreasing to head back towards the left side 
+            bluePos--;
+            // coordinates of the square
+            blueSquare.style.top = bluePos + 'px';
+            blueSquare.style.left = bluePos + 'px';
+        }
+    }
+}
 
