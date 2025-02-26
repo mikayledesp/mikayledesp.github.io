adf409144118d6675af666136c31eb38ef78064f
Mikayle Despaigne <158209632+mikayledesp@users.noreply.github.com>
2025-02-25 22:53:20 -0500

    Done with assignment

---
+++ CSC209/Hwks/Hw4/creative/js/creative.js
@@ -0,0 +1,79 @@
+function moveRed() {   
+    var redSquare = document.getElementById("redSq");   
+    var redPos = 0;
+    var speed = parseInt(document.getElementById("redSpeed").value); 
+   var redStepId = setInterval(stepRed, speed);
+
+    function stepRed() {
+        if (redPos >= 350) {
+            clearInterval(redStepId); 
+        } else {
+            redPos++; 
+            redSquare.style.top = redPos + 'px'; 
+            redSquare.style.left = redPos + 'px';
+        }
+    }
+}
+
+function moveBlue() {  
+    var blueSquare = document.getElementById("blueSq");
+    var bluePos = 350;
+    var speed = parseInt(document.getElementById("blueSpeed").value); 
+
+    var blueStepId = setInterval(stepBlue, speed); 
+
+    function stepBlue() {
+        if (bluePos == 125) {
+            clearInterval(blueStepId); 
+        } else {
+            bluePos--;
+            blueSquare.style.top = bluePos + 'px';
+            blueSquare.style.left = bluePos + 'px';
+        }
+    }
+}
+
+function movePurple() {  
+    var purpleSquare = document.getElementById("purpleSq");
+    var purplePos = 150;
+    var speed = parseInt(document.getElementById("purpleSpeed").value); 
+
+    clearInterval(purpleStepId); 
+    var purpleStepId = setInterval(stepPurple, speed); 
+
+    function stepPurple() {
+        if (purplePos == 0) {
+            clearInterval(purpleStepId); 
+        } else {
+            purplePos--;
+            purpleSquare.style.top = purplePos + 'px';
+            purpleSquare.style.left = purplePos + 'px';
+        }
+    }
+}
+
+function movePink() {  
+    var pinkSquare = document.getElementById("pinkSq");
+    var pinkPos = 0;
+    var speed = parseInt(document.getElementById("pinkSpeed").value); 
+
+    clearInterval(pinkStepId); 
+    var pinkStepId = setInterval(stepPink, speed); 
+
+    function stepPink() {
+        if (pinkPos == 350) {
+            clearInterval(pinkStepId); 
+        } else {
+            pinkPos++;
+            pinkSquare.style.bottom = pinkPos + 'px';
+            pinkSquare.style.left = pinkPos + 'px';
+        }
+    }
+}
+
+function moveAll(){
+    moveBlue();
+    movePink();
+    moveRed();
+    movePurple();
+}
--- CSC209/Hwks/Hw4/technical/Animation/js/animation.js
+++ CSC209/Hwks/Hw4/technical/Animation/js/animation.js
@@ -1,14 +1,12 @@
 function moveRed() {   
     var redSquare = document.getElementById("redSq");   
     var redPos = 0;
-    var speed = parseInt(document.getElementById("redSpeed").value); // Get user-selected speed
-
-    clearInterval(redInterval); // Clear any existing red square animation
-   var redInterval = setInterval(stepRed, speed); // Apply selected speed
+    var speed = parseInt(document.getElementById("redSpeed").value); 
+   var redStepId = setInterval(stepRed, speed);
 
     function stepRed() {
         if (redPos >= 350) {
-            clearInterval(redInterval); // Stop when reaching the limit
+            clearInterval(redStepId); 
         } else {
             redPos++; 
             redSquare.style.top = redPos + 'px'; 
@@ -20,18 +18,62 @@ function moveRed() {
 function moveBlue() {  
     var blueSquare = document.getElementById("blueSq");
     var bluePos = 350;
-    var speed = parseInt(document.getElementById("blueSpeed").value); // Get user-selected speed
+    var speed = parseInt(document.getElementById("blueSpeed").value); 
 
-    clearInterval(blueInterval); // Clear any existing blue square animation
-    var blueInterval = setInterval(stepBlue, speed); // Apply selected speed
+    var blueStepId = setInterval(stepBlue, speed); 
 
     function stepBlue() {
-        if (bluePos <= 0) {
-            clearInterval(blueInterval); // Stop when reaching the limit  
+        if (bluePos == 125) {
+            clearInterval(blueStepId); 
         } else {
             bluePos--;
             blueSquare.style.top = bluePos + 'px';
             blueSquare.style.left = bluePos + 'px';
         }
     }
+}
+
+function movePurple() {  
+    var purpleSquare = document.getElementById("purpleSq");
+    var purplePos = 150;
+    var speed = parseInt(document.getElementById("purpleSpeed").value); 
+
+    clearInterval(purpleStepId); 
+    var purpleStepId = setInterval(stepPurple, speed); 
+
+    function stepPurple() {
+        if (purplePos == 0) {
+            clearInterval(purpleStepId); 
+        } else {
+            purplePos--;
+            purpleSquare.style.top = purplePos + 'px';
+            purpleSquare.style.left = purplePos + 'px';
+        }
+    }
+}
+
+function movePink() {  
+    var pinkSquare = document.getElementById("pinkSq");
+    var pinkPos = 0;
+    var speed = parseInt(document.getElementById("pinkSpeed").value); 
+
+    clearInterval(pinkStepId); 
+    var pinkStepId = setInterval(stepPink, speed); 
+
+    function stepPink() {
+        if (pinkPos == 350) {
+            clearInterval(pinkStepId); 
+        } else {
+            pinkPos++;
+            pinkSquare.style.bottom = pinkPos + 'px';
+            pinkSquare.style.left = pinkPos + 'px';
+        }
+    }
+}
+
+function moveAll(){
+    moveBlue();
+    movePink();
+    moveRed();
+    movePurple();
 }
