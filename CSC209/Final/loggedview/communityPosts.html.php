dd59350ab2f1f812331c6e992dc88d90f5e7d0a0
Mikayle Despaigne <158209632+mikayledesp@users.noreply.github.com>
2025-05-09 14:15:22 -0400

    changed card style

--- CSC209/Final/loggedview/communityPosts.html.php
+++ CSC209/Final/loggedview/communityPosts.html.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Community Posts</title>
   <link href="../bootstrap-5.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../css/indexStyle.css">
+  <link rel="stylesheet" href="../css/comminity.css">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg">
